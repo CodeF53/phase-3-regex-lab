492fb8c8a9e08e03b698804f044c65005b863291
ChaseWelsh <fseusb@gmail.com>
2022-09-21 09:41:07 -0600

    I could do this all day, its kinda fun

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,19 +1,19 @@
 def starts_with_a_vowel?(word)
-
+    word.match?(/^[aeiou]/i)
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+    pp text.scan(/\bun\w*ing\b/i)
 end
 
 def words_five_letters_long(text)
-
+    text.scan(/\b\w{5}\b/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+    text.match?(/^[A-Z].*[\.\?\!]$/)
 end
 
 def valid_phone_number?(phone)
-
+    phone.match?(/^\D*(\d{3}\D*){2}\d{4}$/)
 end
